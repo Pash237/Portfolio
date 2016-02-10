94e5e64071d607486b89afb9575208a1654c4ba8
Pavel Alexeev <pasha.alexeev@gmail.com>
2016-02-10 15:38:17 +0300

    Current year instead of “2016”

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,7 +9,7 @@ class Footer extends Component {
 				<a href="https://www.instagram.com/Pash237/"><img className="footer-button" src="button_instagram.png"/></a>
 				<a href="https://vk.com/pavel.alexeev"><img className="footer-button" src="button_vk.png"/></a>
 				<a href="mailto:pasha.alexeev@gmail.com"><img className="footer-button" src="button_email.png"/></a>
-				<p className="footer-copyright">PAVEL ALEXEEV © 2016</p>
+				<p className="footer-copyright">PAVEL ALEXEEV © {new Date().getFullYear()}</p>
 			</div>
 		);
 	}
