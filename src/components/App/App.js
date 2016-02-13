f7c31fca9f0d31a893e887bc9c4de093952fb3b3
Pavel Alexeev <pasha.alexeev@gmail.com>
2016-02-13 13:31:50 +0300

    Google Analytics with 'react-ga'

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,11 +1,22 @@
 import React, { Component, PropTypes } from 'react';
 import { browserHistory, Router, Route, IndexRoute, Link } from 'react-router';
-import GoogleAnalytics from '../GoogleAnalytics';
+import ga from 'react-ga';
+import config from '../../config'
 import TitlePage from '../TitlePage';
 import ProjectPage from '../ProjectPage';
 import './style.scss';
 
 class App extends Component {
+
+	constructor() {
+		super();
+
+		ga.initialize(config.googleAnalyticsId, { debug: false });
+
+		browserHistory.listen(function(event) {
+			ga.pageview(event.pathname);
+		});
+	}
 
 	render() {
 		return (
@@ -16,7 +27,6 @@ class App extends Component {
 						<Route path=":project" component={ProjectPage} />
 					</Route>
 				</Router>
-				<GoogleAnalytics />
 			</div>
 		);
 	}
